97cc1a1080c2676d307089060780c2e6807aafbc
ruby116220 <92256094+ruby116220@users.noreply.github.com>
2021-10-10 20:40:02 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,25 @@
+import os
+import utils
+import amc2pyl
+
+
+
+
+
+# name = 'HDM_bd_cartwheelLHandStart1Reps_001_120.amc'
+# action_name = utils.getAction(name)
+# actor_name = utils.getActor(name)
+# actor_asf = 'HDM_{}.asf'.format(actor_name)
+# print(os.path.join(os.path.join(data_path, action_name), actor_asf))
+
+
+
+
+# print(utils.genActionClass()utils.getAction(name))
+# data = utils.read_xyz(asf_path, amc_path)
+# print(data.shape[1])
+# print("frame：1，joint：1  =={}".format(data[:, 0, 0, 0]))
+# print("frame：1，joint：2  =={}".format(data[:, 0, 1, 0]))
+# print("frame：1，joint：3  =={}".format(data[:, 0, 2, 0]))
+# print("frame：2，joint：1  =={}".format(data[:, 1, 0, 0]))
+# print("frame：2，joint：2  =={}".format(data[:, 1, 1, 0]))
